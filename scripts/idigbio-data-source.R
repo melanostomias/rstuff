66034f2589cd0ffbbcbd7c50cd9df8c0fe7c5c06
kevinlove <kevinlove@ufl.edu>
2017-08-11 13:28:07 -0400

    Code clean-up and documentation. Added log file that was never written during the last build.

--- scripts/idigbio-data-source.R
+++ scripts/idigbio-data-source.R
@@ -7,20 +7,25 @@ library(progress)
 
 
 ##########################################################
-# This script will build two large dataframes from the collections 
-# and recordsets created by 'identifying-collections.R'
+# This script will build a large dataframe of specimen records
+# from the collections and recordsets created by 'identifying-collections.R'
+#
+# 
 # Inputs: fishy-recordsets.rdata & centrarchidae-recordsets.rdata
-# Output: data-raw/big-ole-dataframe.rdata & data-raw/big-ole-dataframe1.rdata
+# Output: data-raw/big-ole-dataframe1.rdata
 
 
 ## First we need to see where the fishy sets and cent sets overlap
 load("data-raw/fishy-recordsets.rdata")
 load("data-raw/centrarchidae-recordsets.rdata")
 
-##These are our 'completeness' checks
-## Are all of the ASIH codes present in both datasets and are all the recordset UUID's present in both sets?
+## These are our 'completeness' checks:
+## Are all of the ASIH codes present in both datasets? 
+## Are all the recordset UUID's present in both sets?
 all(all(bd$ASIHCode %in% gd$ASIHCode)&&all(bd$recordset_uuid %in% gd$idigbio.recordset))
-##IF False (fixes KUIT Problem)
+
+## This should have returned false, as we know that KUIT is not present
+## Let's fix the KUIT problem now
 rr <- bd[!bd$ASIHCode %in% gd$ASIHCode,]
 rr$idigbio.recordset <- rr$recordset_uuid
 rr$name <- NULL
@@ -28,9 +33,15 @@ rr$recordset_uuid <- NULL
 gd <- rbind.fill(gd,rr)
 
 
-## We need to make sure we have all of the possible code/recorset combos possible
+## We need to make sure we have all of the possible 
+## collection code/recorset combos possible. We can
+## use the information we have and the ridigbio package
+## to investigate this further
 test <- gd
 logDF <- data.frame()
+
+## For every known recordset, build a dataframe of 
+## collection codes present in the data 
 for (i in 1:length(gd$idigbio.recordset)){
         cDF <- idig_top_records(rq=list(recordset=test$idigbio.recordset[i]),top_fields = "collectioncode")
         cDF <- data.frame(ASIHCode=test$ASIHCode[i],dwc.collectionCode=names(cDF$collectioncode),idigbio.recordset=test$idigbio.recordset[i],dwc.institutionCode=test$dwc.institutionCode[i])
@@ -39,17 +50,28 @@ for (i in 1:length(gd$idigbio.recordset)){
         }
 }
 logDF <- logDF[!duplicated(logDF),]
+## Apply our known ASIH codes to these recordsets
 jj <- read.csv("https://raw.githubusercontent.com/melanostomias/rstuff/master/vocab/ASIH_Codes_UUID.csv",stringsAsFactors = F)
 jj <- merge(logDF,jj,by.x = "ASIHCode")
+## Remove the duplicates
 jj <- jj[!duplicated(jj),]
+## Write this dataframe out so we can examine it and build our include list
+write.csv(jj,file = paste0("../log/all-collectionCode-recordsets-",format(Sys.time(), "%Y-%m-%d-%H-%M-%S"),".csv"),row.names = FALSE)
 
+## Here we have a known list of collection codes to icludes. This list was 
+## created by verifying the recordset/collection code contains fish from a US collection
+## todo: Make this list available as a CSV to read-in
 inlcudes <- c("i","fish","ich","ic","fishes","ichthyology","larval collection","adult collection","egg collection","juvenile collection","skeleton collection")
 jj <- jj[jj$dwc.collectionCode %in% inlcudes,]
 jj <- jj[!jj$dwc.collectionCode %in% gd$dwc.collectionCode,]
 jj$recordset_uuid <- NULL
 gd <- rbind(gd,jj)
 
-##Fix up some more of this KUIT monkey business
+
+## Fix up some more of this KUIT monkey business
+## We need to find out what the collection code and instution codes are in the data
+## and make sure they are in our dataframe
+
 kuit <- gd[gd$ASIHCode=="KUIT",]$idigbio.recordset
 kuColCode <- idig_top_records(rq=list(recordset=kuit),top_fields = "collectioncode")
 kuInsCode <- idig_top_records(rq=list(recordset=kuit),top_fields = "institutioncode")
@@ -60,8 +82,9 @@ ku$dwc.institutionCode <- names(kuInsCode$institutioncode)
 gd <- rbind(gd,ku)
 gd <- gd[!gd$ASIHCode=="KUIT",]
 
-##Fix up the UMMZ monkey business
-##this code will need to be deleted once we get this data from iDigBio
+## Fix up the UMMZ monkey business
+## This code will need to be deprecated once we get this data from the
+## iDigBio API
 uMMz <- gd[gd$ASIHCode=="UMMZ",]
 uMMz$idigbio.recordset <- "168d73e6-8e1a-4b69-9d8b-96ff2de773ee"
 uMMz$dwc.collectionCode <- "fishes"
@@ -69,19 +92,14 @@ uMMz$dwc.institutionCode <- "ummz"
 gd <- rbind(gd,uMMz)
 
 
-##Let's build an idigbio dataframe from our centrarchid recordsets
-## using the download system
+## Let's build an idigbio dataframe from our centrarchid recordsets
+## using the iDigBio download system. We can use our dataframe to specify
+## the data queries we are interested in.
 
-# #quick count test
-# for (i in 1:length(gd$dwc.institutionCode)){
-#         rq <- list("institutioncode"=gd$dwc.institutionCode[i],"collectioncode"=gd$dwc.collectionCode[i],"recordset"=gd$idigbio.recordset[i],"data.dwc:basisOfRecord"="PreservedSpecimen")
-#         f <- idig_count_records(rq)
-#         print(f)
-# }
-
-
+## Build the record query in the format the API expects
 gd$rq <- sapply(1:length(gd$idigbio.recordset),function(x) toJSON(list("collectioncode"=gd$dwc.collectionCode[x],"recordset"=gd$idigbio.recordset[x])))
-
+## Use the download system to fetch the files we are interested in and 
+## save them to a directory for each ASIH code
 for (i in 1:length(gd$dwc.institutionCode)) {
         rq <- gd$rq[i]
         email <- "kevinlove@ufl.edu"
@@ -108,9 +126,9 @@ for (i in 1:length(gd$dwc.institutionCode)) {
         
 }
 
+## Build a vector of the ASIH codes we downloaded data for 
 folderVector <- list.dirs("data-raw/idb-download/centDL",recursive = F)
-#fileVector <- list.files("data-raw/idb-download/centDL/",recursive = T)
-
+## Unzip the files in each ASIH directory
 for (i in 1:length(folderVector)){
         fileVector <- list.files(folderVector[i],recursive = T)
         for(ii in 1:length(fileVector)){
@@ -119,8 +137,8 @@ for (i in 1:length(folderVector)){
         }
 }
 
-##fileVector <- list.files("data-raw/idb-download/centDL/",recursive = T)
-##fileVector <- strtrim(fileVector,36)
+## Now let's loop through each directory and append the iDigBio indexed
+## occurence data found in each ASIH directory
 gigaCentDF <- data.frame()
 for (i in 1:length(folderVector)){
         datafiles <- list.dirs(folderVector[i],recursive = F)
@@ -130,93 +148,8 @@ for (i in 1:length(folderVector)){
         }
 }        
 
-
-##Gotta add the ASIH code now
+## We can now add the ASIH code to each known collection record
 gigaCentDF <- merge(gigaCentDF,gd,by=c("idigbio.recordset","dwc.collectionCode","dwc.institutionCode"),all.x=TRUE)
 
+## Save this large dataframe to a file
 save(gigaCentDF, file = 'data-raw/big-ole-dataframe1.rdata', compress = 'xz')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#########################################################################################################
-### All ye who pass this point are doomed to repeat their mistakes#######################################
-
-
-
-
-
-## This loop uses the iDigBio download system to generate zip files for each fishy colleciton
-for (i in 1:length(bd$recordset_uuid)) {
-        rq <- list("recordset"= bd$recordset_uuid[i])
-        gq <-toJSON(rq,auto_unbox = T)
-        email <- "kevinlove@ufl.edu"
-        createDownload <- paste0("https://api.idigbio.org/v2/download/?rq=",URLencode(gq),"&email=",email)
-        ##Create the download
-        rqDL <- fromJSON(createDownload)
-        ##Check the status
-        rqStatus <- fromJSON(rqDL$status_url)$task_status
-        ##Loop until complete (24 hours sounds nice)
-        for (v in 1:86400){
-                if(!rqStatus=="SUCCESS"){
-                        print(paste0("waiting for success ",v))
-                        rqStatus <<- fromJSON(rqDL$status_url)$task_status
-                        Sys.sleep(1)
-                }else{ getData <- fromJSON(rqDL$status_url)$download_url
-                break}
-        }
-        ##Download the data
-        download.file(getData,destfile = paste0("data-raw/idb-download/fishy-sets/",bd$recordset_uuid[i],".zip"))
-        
-}
-
-
-## Create a vector of all downloaded fishy recordsets
-## unzip them into new directories
-fileVector <- list.files("data-raw/idb-download/fishy-sets/")        
-for (i in 1:length(fileVector)){
-        unzip(paste0("data-raw/idb-download/fishy-sets/",fileVector[i]),exdir =paste0("data-raw/idb-download/fishy-sets/",strtrim(fileVector[i],36)))
-        print(paste0("unzipping file # ",i))
-}
-
-##Build a dataframe from the downloaded datasets
-##This is a hack for now
-fileVector <- strtrim(fileVector,36)
-rsSets <- unique(gd$idigbio.recordset)
-fileVector <- setdiff(fileVector,rsSets)
-####################################################
-
-gigaDF <- data.frame()
-for (i in 1:length(fileVector)){
-        tmpDF <- read.csv(paste0("data-raw/idb-download/fishy-sets/",fileVector[i],"/occurrence.csv"),stringsAsFactors = F)
-        gigaDF <- rbind.fill(gigaDF,tmpDF)
-}
- 
-
-
-## Gotta add the ASIH codes at some point
-bd$name <- NULL
-gigaDF <- merge(gigaDF,bd, by.x="idigbio.recordset",by.y = "recordset_uuid",all.x = T)
-
-## Save the resulting dataframe
-save(gigaDF, file = 'data-raw/big-ole-dataframe.rdata', compress = 'xz')
-
-
