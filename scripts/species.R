02478f884ce3bbfa249d0aef85af12b677c2d2ea
kevinlove <kevinlove@ufl.edu>
2017-08-14 13:14:35 -0400

    Code clean-up and documentation

--- scripts/species.R
+++ scripts/species.R
@@ -1,12 +1,21 @@
-##Load Libraries
 library(plyr)
 library(tidyr)
 library(jsonlite)
 library(dplyr)
 library(plotly)
 
+######################################################################
+## This script is used to clean up the outputs of 'build-data-frame.R'
+## and do more work on dwc.scientificName & dwc.specificEpithet data
+##
+## Inputs: 'data-raw/workingDF.rdata'
+## Outputs: 'raw-speciesDF.rdata'
 
-##Build a dataframe from the downloaded fishy datasets
+
+## Build a dataframe from the raw occurence data downloaded previously
+## Since we are only interested in working with the specificEpithet and 
+## scientificName, we will only look at that data now
+
 folderVector <- list.dirs("data-raw/idb-download/centDL",recursive = F)
 gigaDF <- data.frame()
 for (i in 1:length(folderVector)){
@@ -18,17 +27,26 @@ for (i in 1:length(folderVector)){
         gigaDF <- rbind.fill(gigaDF,tmpDF)
         }
 }
+
+## Write this raw species data to a file
 save(gigaDF,file = "data-raw/raw-speciesDF.rdata",compress = "bzip2")
-##load("data-raw/raw-speciesDF.rdata")
+
+## We can load the dataframe from a file
+## load("data-raw/raw-speciesDF.rdata")
+## or we can work from the object we have already created.
+
+## For now, we're gonna create a new object and name the variables
+## we are interested in
 species <- gigaDF
 names(species) <- c("idigbio.uuid","dwc.specificEpithet","dwc.scientificName")
 
-## Match everything back
-##load("data-raw/raw-speciesDF.rdata")
+## We can now match our species data back to our working dataframe
+## load("data-raw/raw-speciesDF.rdata")
 load("data-raw/workingDF.rdata")
 workingDF <- merge(workingDF,species,all.x = T)
 
-
+## We can write the data we find about each collection to a 
+## file in a directory
 specASIH <- unique(workingDF$ASIHCode)
 for(i in 1:length(specASIH)){
         dir.create("../data",showWarnings = F)
@@ -39,17 +57,10 @@ for(i in 1:length(specASIH)){
         names(rawSpecsDF) <- c("dwc.scientificName","freq")
         rawSpecsDF <- rawSpecsDF[order(rawSpecsDF$freq,decreasing = T),]
         write.csv(rawSpecsDF, file = paste0("../data/",specASIH[i],"/data/",specASIH[i],"-RAW_species.csv"),row.names = FALSE)
-        ##Get on with the show
-        # specsDF <- plyr::count(workingDF[workingDF$ASIHCode==specASIH[i],],"dwc.specificEpithet")
-        # write.csv(prepsDF, file = paste0("../data/",prepASIH[i],"/data/",prepASIH[i],"-standardPreps.csv"),row.names = FALSE)
-        # p <- plot_ly(prepsDF, labels = ~standardPrep, values = ~freq, type = "pie") %>%
-        #         layout(title = paste0(prepASIH[i]," Standard Preparations"),
-        #                xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-        #                yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-        # export(p,file =paste0("../data/",prepASIH[i],"/figures/",prepASIH[i],"-standardPreps-PLOTLY.png"),vheight = 1080 )
-        
-        
 }
+
+## We can also build summary data files and
+## write those to a summary directory
 
 aggSpecsDF <- plyr::count(workingDF,c("ASIHCode","dwc.scientificName"))
 aggSpecsDF$freq <- NULL
@@ -60,7 +71,8 @@ dir.create("../data/summary/data",showWarnings = F)
 dir.create("../data/summary/figures",showWarnings = F)
 write.csv(aggSpecsDF, file = "../data/summary/data/asih-by-species.csv",row.names = FALSE)
 
-##Plot our aggregated species counts
+## Plot our aggregated species counts for the aggregate and
+## output to a summary directory
 aggSpecsDF <- aggSpecsDF[order(aggSpecsDF$freq,decreasing = T),]
 x <- list(title = "ASIH Code",
           type = "category",
