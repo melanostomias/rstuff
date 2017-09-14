f61c265a0ef1df606efc0a1befaaeadcd20da223
kevinlove <kevinlove@ufl.edu>
2017-09-14 16:28:50 -0400

    Build plots for mediarecords

--- scripts/mediaRecords.R
+++ scripts/mediaRecords.R
@@ -1,3 +1,4 @@
+library(plotly)
 ## mediarecords
 folderVector <- list.dirs("data-raw/idb-download/centDL",recursive = F)
 mDF <- data.frame()
@@ -17,3 +18,20 @@ mDF <- mDF[order(mDF$Count, decreasing = T),]
 mDF$ASIHCode <- gsub("data-raw/idb-download/centDL/", "", mDF$ASIHCode)
 
 write.csv(mDF,file = "../data/summary/data/asih-by-mediarecords.csv",row.names = F)
+
+
+##Plot
+
+x <- list(title = "ASIH Code",
+          type = "category",
+          categoryorder = "array",
+          categoryarray = sort(mDF$Count,decreasing = T))
+y <- list(title = "Media Records")
+p5 <- plot_ly(
+        data = mDF,
+        x = ~ASIHCode,
+        y = ~Count,
+        type = "bar")%>%
+        layout(xaxis = x,yaxis=y)
+p5
+export(p5,file ="../data/summary/figures/asih-by-mediarecords-PLOTLY.png",vheight = 1080 )
